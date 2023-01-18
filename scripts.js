747227ef9fc6ece97a64f788b8e3248656d5d8ef
Дмитрий Шарапендо <dimagen@192.168.0.127>
2023-01-19 00:11:10 +0300

    my first commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+"use strict"; // не допускает неточностей в написании кода
+let numbebr = 5;
+const leftBorderWidth = 1;
+
+const answer = [];
+answer[0] = prompt ('как ваши дела?', '');
+answer[1] = prompt ('как вас зовут?', '');
+answer[2] = prompt ('сколько вам лет?', '');
+
+document.write (answer);
+
